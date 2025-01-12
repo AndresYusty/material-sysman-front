e11737bb4440cbc47ac0b403aa33bd794581e974
AndresYusty <andrescardoso1002@gmail.com>
2025-01-12 00:16:51 -0500

    init proyect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth', loadComponent: () => import('./auth/login/login.component').then((c) => c.LoginComponent) },
+  { path: 'materials', loadComponent: () => import('./materials/materials.component').then((c) => c.MaterialsComponent) },
+  { path: 'locations', loadComponent: () => import('./locations/locations.component').then((c) => c.LocationsComponent) },
+  { path: '**', redirectTo: 'auth' },
+];
